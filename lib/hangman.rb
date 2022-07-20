a278f65d9fb431dbe846687abdcf6caf8ee013fd
ct1559 <e.cooper.taylor@gmail.com>
2022-07-20 09:41:32 -0400

    Add game logic
    Add logic to take user letter and word guesses. The
    guesses are compared with the randomly selected word
    and game_board is updated if guessed letter is in word.
    Game is over with word is guessed correctly, all letters
    have been guessed, or user run out of guesses.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,15 +1,97 @@
 require_relative 'board'
+require 'colorize'
 
 # Hangman class is reposible for running game logic
 class Hangman
-	def initialize 
-		@word = ''
-		@board = Board.new(@word)
-		@guess_count = 0
-		@letters_guessed = []
-		@game_over = false
-	end
+  def initialize(word, board, guess_count, letters_guessed)
+    @word = word
+    @board = board
+    @guess_count = guess_count
+    @letters_guessed = letters_guessed
+    @game_over = false
+  end
 
-	def play
-	end
+  def play
+    puts @word
+
+    while @guess_count < 6 && @game_over == false
+      puts "\nGuess Count: ".green + @guess_count.to_s + '   Remaining Guesses: '.green + (6 - @guess_count).to_s
+      puts 'Letters that have been guessed: '.green + "#{@letters_guessed}\n"
+      @board.game_board.each { |element| print element }
+      puts "\n\n"
+      guess = letter_or_word
+      # @letters_guessed.push(guess)
+      if guess.length == 1
+        check_letter(guess)
+      else
+        check_word(guess)
+      end
+      @guess_count += 1
+      unless @board.game_board.include?(' _ ')
+        puts "\nYou've found all the letters of the word!".green
+        @game_over = true
+      end
+    end
+    @board.game_board.each { |element| print element }
+    puts "\n\n"
+    return if @game_over == true
+
+    puts 'You were unable to correctly guess it in 6 tries :(. The word was: ' + @word.to_s.green
+  end
+
+  def letter_or_word
+    puts 'Input "1" to guess letter, input "2" to guess the word:'.yellow
+    user_input = gets.chomp
+    case user_input
+    when '1'
+      guess_letter
+    when '2'
+      guess_word
+    else
+      puts 'Invalid input, try again'.red
+      letter_or_word
+    end
+  end
+
+  def guess_letter
+    puts "\nGuess a letter:".yellow
+    user_input = gets.chomp.downcase
+    if /[a-z]/.match?(user_input) == false || user_input.length > 1
+      puts 'Invalid input, try again'.red
+      return guess_letter
+    elsif @letters_guessed.include?(user_input)
+      puts "You've already guessed #{user_input}, try again".red
+      return guess_letter
+    end
+    @letters_guessed.push(user_input)
+    user_input
+  end
+
+  def guess_word
+    puts "\nGuess the word:".yellow
+    user_input = gets.chomp.downcase
+    if user_input.length != @word.length
+      puts 'Invalid word length, try again'.red
+      guess_word
+    end
+    user_input
+  end
+
+  def check_letter(letter)
+    @word.chars.each_with_index do |char, index|
+      if letter == char
+        @board.update_board(letter, index)
+      end
+    end
+  end
+
+  def check_word(word)
+    if word == @word
+      puts 'You correctly guessed the word!'.green
+      @game_over = true
+    else
+      puts 'That was not the correct word'.orange
+    end
+  end
+
 end
--- lib/main.rb
+++ lib/main.rb
@@ -2,24 +2,38 @@ require_relative 'board'
 require_relative 'hangman'
 
 def start_game
-	word = get_word
-	board = Board.new(word)
-	board.game_board.each { |element| print element }
-	puts "\n\n"
-	puts word
+  word = rand_word
+  board = Board.new(word)
+  game = Hangman.new(word, board, 0, [])
+  game.play
+  play_again
 end
 
-def get_word
- contents = File.readlines('google-10000-english-no-swears.txt')
- list = []
- contents.each { |line| list.push(line.chomp) }
- filtered_list = filter_word_list(list)
- random_index = rand(filtered_list.length)
- filtered_list[random_index]
+def rand_word
+  contents = File.readlines('google-10000-english-no-swears.txt')
+  list = []
+  contents.each { |line| list.push(line.chomp) }
+  filtered_list = filter_word_list(list)
+  random_index = rand(filtered_list.length)
+  filtered_list[random_index]
 end
 
 def filter_word_list(list)
-	list.select { |word| word.length.between?(5, 12) }
+  list.select { |word| word.length.between?(5, 12) }
+end
+
+def play_again
+  puts "\nWould you like to play again? Enter 'y' for yes or 'n' for no"
+  user_input = gets.chomp.downcase
+  case user_input
+  when 'y'
+    start_game
+  when 'n'
+    puts "\nGoodbye!"
+  else
+    puts 'Invalid input, try again'
+    play_again
+  end
 end
 
 start_game
